1fbc3eb61df950c479e323a18698e60918dcb025
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-11-22 01:01:05 +0000

    added file

---
+++ nested.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Lucas LeBlanc
+//
+// Created by: Lucas LeBlanc
+// Created on: Nov 2022
+// This program uses nested loops to print RGB values
+
+#include <iostream>
+
+int main() {
+    // This program uses nested loops to print RGB values
+
+    int rgb1;
+    int rgb2;
+    int rgb3;
+
+    // Process and Output
+    for (rgb1 = 0; rgb1 < 256; rgb1++) {
+        for (rgb2 = 0; rgb2 < 256; rgb2++) {
+            for (rgb3 = 0; rgb3 < 256; rgb3++) {
+                std::cout << "\nRGB(" << rgb1 << ","
+                          << rgb2 << "," << rgb3 << ")" << std::endl;
+            }
+        }
+    }
+
+    std::cout << "\nDone" << std::endl;
+}
